b722af64825704560b1ccfdc9e2dab80a35ca32d
Manjiri Acharekar <msa530@nyu.edu>
2018-05-06 08:01:37 -0400

    Delete post, saved posts and comments files

---
+++ includes/deletepost.php
@@ -0,0 +1,21 @@
+<?php
+include("../config/connect.php");
+$posts_id = htmlentities($_POST['pid'], ENT_QUOTES);
+$delete_post_sql = "DELETE FROM Post WHERE post_id= :posts_id";
+$delete_post = $conn->prepare($delete_post_sql);
+$delete_post->bindParam(':posts_id',$posts_id,PDO::PARAM_INT);
+$delete_post->execute();
+// Delete notification 'like'
+$notifyType = "like";
+$sendNotification = $conn->prepare("DELETE FROM Notifications WHERE notifyType_id=:notifyType_id AND notifyType=:notifyType");
+$sendNotification->bindParam(':notifyType_id',$posts_id,PDO::PARAM_INT);
+$sendNotification->bindParam(':notifyType',$notifyType,PDO::PARAM_STR);
+$sendNotification->execute();
+// Delete notification 'comment'
+$notifyType = "comment";
+$sendNotification = $conn->prepare("DELETE FROM Notifications WHERE notifyType_id=:notifyType_id AND notifyType=:notifyType");
+$sendNotification->bindParam(':notifyType_id',$posts_id,PDO::PARAM_INT);
+$sendNotification->bindParam(':notifyType',$notifyType,PDO::PARAM_STR);
+$sendNotification->execute();
+// ==================================
+?>
