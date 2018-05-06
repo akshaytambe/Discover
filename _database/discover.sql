37b04507087ab83e366462d743cc8e168c38416e
Manjiri Acharekar <msa530@nyu.edu>
2018-05-06 07:11:34 -0400

    Updating DDL Queries

--- _database/discover.sql
+++ _database/discover.sql
@@ -1,9 +1,31 @@
--- Authors: Akshay Tambe (apt321@nyu.edu) | Manjiri Acharekar (msa530@nyu.edu)
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 17, 2017 at 08:14 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `discover`
+--
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `Comments`
+--
+
 CREATE TABLE `Comments` (
   `c_id` bigint(50) NOT NULL,
   `c_author_id` bigint(11) NOT NULL,
@@ -15,15 +37,23 @@ CREATE TABLE `Comments` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
--- Table structure for table `Follow`
-CREATE TABLE `Follow` (
+
+--
+-- Table structure for table `Following_Friends`
+--
+
+CREATE TABLE `Following_Friends` (
   `id` bigint(11) NOT NULL,
   `uf_one` bigint(11) NOT NULL,
   `uf_two` bigint(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
+
+--
 -- Table structure for table `Likes`
+--
+
 CREATE TABLE `Likes` (
   `id` bigint(11) NOT NULL,
   `liker` bigint(11) NOT NULL,
@@ -31,7 +61,11 @@ CREATE TABLE `Likes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
+
+--
 -- Table structure for table `Messages`
+--
+
 CREATE TABLE `Messages` (
   `id` int(11) NOT NULL,
   `m_id` bigint(11) NOT NULL,
@@ -43,7 +77,11 @@ CREATE TABLE `Messages` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
+
+--
 -- Table structure for table `DiaryNotes`
+--
+
 CREATE TABLE `DiaryNotes` (
   `main_id` int(11) NOT NULL,
   `id` bigint(20) NOT NULL,
@@ -54,7 +92,11 @@ CREATE TABLE `DiaryNotes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
+
+--
 -- Table structure for table `Notifications`
+--
+
 CREATE TABLE `Notifications` (
   `id` int(11) NOT NULL,
   `n_id` bigint(11) NOT NULL,
@@ -67,16 +109,24 @@ CREATE TABLE `Notifications` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
--- Table structure for table `FavFriend`
-CREATE TABLE `FavFriend` (
+
+--
+-- Table structure for table `Favorite_Friend`
+--
+
+CREATE TABLE `Favorite_Friend` (
   `id` bigint(11) NOT NULL,
   `u_id` bigint(11) NOT NULL,
   `p_id` bigint(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
--- Table structure for table `SavedPost`
-CREATE TABLE `SavedPost` (
+
+--
+-- Table structure for table `Saved_Post`
+--
+
+CREATE TABLE `Saved_Post` (
   `main_id` int(11) NOT NULL,
   `id` bigint(20) NOT NULL,
   `post_id` bigint(11) NOT NULL,
@@ -85,8 +135,12 @@ CREATE TABLE `SavedPost` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
--- Table structure for table `Register`
-CREATE TABLE `Register` (
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE `Users` (
   `main_id` int(11) NOT NULL,
   `id` bigint(50) NOT NULL,
   `Fullname` varchar(1000) CHARACTER SET utf8mb4 NOT NULL,
@@ -113,7 +167,11 @@ CREATE TABLE `Register` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
+
+--
 -- Table structure for table `Feedback`
+--
+
 CREATE TABLE `Feedback` (
   `id` int(11) NOT NULL,
   `r_id` bigint(11) NOT NULL,
@@ -129,15 +187,23 @@ CREATE TABLE `Feedback` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
--- Table structure for table `Typing`
-CREATE TABLE `Typing` (
+
+--
+-- Table structure for table `Typing_Message`
+--
+
+CREATE TABLE `Typing_Message` (
   `id` bigint(11) NOT NULL,
   `t_from` bigint(11) NOT NULL,
   `t_to` bigint(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
+
+--
 -- Table structure for table `Post`
+--
+
 CREATE TABLE `Post` (
   `post_id` bigint(50) NOT NULL,
   `author_id` bigint(11) NOT NULL,
@@ -150,100 +216,141 @@ CREATE TABLE `Post` (
   `shared` varchar(1000) CHARACTER SET utf8mb4 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+--
 -- Indexes for dumped tables
--- --------------------------------------------------------
+--
+
+--
 -- Indexes for table `Comments`
+--
 ALTER TABLE `Comments`
   ADD PRIMARY KEY (`c_id`);
--- --------------------------------------------------------
--- Indexes for table `Follow`
-ALTER TABLE `Follow`
+
+--
+-- Indexes for table `Following_Friends`
+--
+ALTER TABLE `Following_Friends`
   ADD PRIMARY KEY (`id`);
--- ------------------------------------------------------
+
+--
 -- Indexes for table `Likes`
+--
 ALTER TABLE `Likes`
   ADD PRIMARY KEY (`id`);
--- ------------------------------------------------------
+
+--
 -- Indexes for table `Messages`
+--
 ALTER TABLE `Messages`
   ADD PRIMARY KEY (`id`);
--- ------------------------------------------------------
+
+--
 -- Indexes for table `DiaryNotes`
+--
 ALTER TABLE `DiaryNotes`
   ADD PRIMARY KEY (`main_id`);
--- ------------------------------------------------------
+
+--
 -- Indexes for table `Notifications`
+--
 ALTER TABLE `Notifications`
   ADD PRIMARY KEY (`id`);
--- ------------------------------------------------------
--- Indexes for table `FavFriend`
-ALTER TABLE `FavFriend`
+
+--
+-- Indexes for table `Favorite_Friend`
+--
+ALTER TABLE `Favorite_Friend`
   ADD PRIMARY KEY (`id`);
--- ------------------------------------------------------
--- Indexes for table `SavedPost`
-ALTER TABLE `SavedPost`
+
+--
+-- Indexes for table `Saved_Post`
+--
+ALTER TABLE `Saved_Post`
   ADD PRIMARY KEY (`main_id`);
--- ------------------------------------------------------
--- Indexes for table `Register`
-ALTER TABLE `Register`
+
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
   ADD PRIMARY KEY (`main_id`);
--- ------------------------------------------------------
+
+--
 -- Indexes for table `Feedback`
+--
 ALTER TABLE `Feedback`
   ADD PRIMARY KEY (`id`);
--- ------------------------------------------------------
--- Indexes for table `Typing`
-ALTER TABLE `Typing`
+
+--
+-- Indexes for table `Typing_Message`
+--
+ALTER TABLE `Typing_Message`
   ADD PRIMARY KEY (`id`);
--- ------------------------------------------------------
+
+--
 -- Indexes for table `Post`
+--
 ALTER TABLE `Post`
   ADD PRIMARY KEY (`post_id`);
 
--- ------------------------------------------------------
+--
 -- AUTO_INCREMENT for dumped tables
--- ------------------------------------------------------
+--
+
+--
 -- AUTO_INCREMENT for table `Comments`
+--
 ALTER TABLE `Comments`
   MODIFY `c_id` bigint(50) NOT NULL AUTO_INCREMENT;
--- ------------------------------------------------------
--- AUTO_INCREMENT for table `Follow`
-ALTER TABLE `Follow`
+--
+-- AUTO_INCREMENT for table `Following_Friends`
+--
+ALTER TABLE `Following_Friends`
   MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
--- ------------------------------------------------------
+--
 -- AUTO_INCREMENT for table `Likes`
+--
 ALTER TABLE `Likes`
   MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT;
--- ------------------------------------------------------
+--
 -- AUTO_INCREMENT for table `Messages`
+--
 ALTER TABLE `Messages`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
--- ------------------------------------------------------
+--
 -- AUTO_INCREMENT for table `DiaryNotes`
+--
 ALTER TABLE `DiaryNotes`
   MODIFY `main_id` int(11) NOT NULL AUTO_INCREMENT;
--- ------------------------------------------------------
+--
 -- AUTO_INCREMENT for table `Notifications`
+--
 ALTER TABLE `Notifications`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
--- ------------------------------------------------------
--- AUTO_INCREMENT for table `FavFriend`
-ALTER TABLE `FavFriend`
+--
+-- AUTO_INCREMENT for table `Favorite_Friend`
+--
+ALTER TABLE `Favorite_Friend`
   MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
--- ------------------------------------------------------
--- AUTO_INCREMENT for table `SavedPost`
-ALTER TABLE `SavedPost`
+--
+-- AUTO_INCREMENT for table `Saved_Post`
+--
+ALTER TABLE `Saved_Post`
   MODIFY `main_id` int(11) NOT NULL AUTO_INCREMENT;
--- ------------------------------------------------------
--- AUTO_INCREMENT for table `Register`
-ALTER TABLE `Register`
+--
+-- AUTO_INCREMENT for table `Users`
+--
+ALTER TABLE `Users`
   MODIFY `main_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
--- ------------------------------------------------------
+--
 -- AUTO_INCREMENT for table `Feedback`
+--
 ALTER TABLE `Feedback`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
--- ------------------------------------------------------
--- AUTO_INCREMENT for table `Typing`
-ALTER TABLE `Typing`
+--
+-- AUTO_INCREMENT for table `Typing_Message`
+--
+ALTER TABLE `Typing_Message`
   MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
