0bdb5c36e87ab376b5b31cdd3ccee489c69b83a3
Akshay Tambe <apt321@nyu.edu>
2018-05-06 08:16:17 -0400

    Updating Include Header Files

--- includes/deleteSavedPost.php
+++ includes/deleteSavedPost.php
@@ -1,8 +1,8 @@
-<?php
-include "../config/connect.php";
-$sid = filter_var(htmlspecialchars($_POST['sid']),FILTER_SANITIZE_NUMBER_INT);
-$deleteS_sql = "DELETE FROM Saved_Post WHERE id = ?";
-$parameter = array($sid);
-$deleteS = $conn->prepare($deleteS_sql);
-$deleteS->execute($parameter);
+<?php
+include "../config/connect.php";
+$sid = filter_var(htmlspecialchars($_POST['sid']),FILTER_SANITIZE_NUMBER_INT);
+$deleteS_sql = "DELETE FROM Saved_Post WHERE id = ?";
+$parameter = array($sid);
+$deleteS = $conn->prepare($deleteS_sql);
+$deleteS->execute($parameter);
 ?>
--- includes/deletecomm.php
+++ includes/deletecomm.php
@@ -1,11 +1,11 @@
-<?php
-include("../config/connect.php");
-session_start();
-$sid = $_SESSION['id'];
-$c_id = htmlentities($_POST['cid'], ENT_QUOTES);
-$p_id = htmlentities($_POST['pid'], ENT_QUOTES);
-$delete_comm_sql = "DELETE FROM Comments WHERE c_id= :c_id";
-$delete_comm = $conn->prepare($delete_comm_sql);
-$delete_comm->bindParam(':c_id',$c_id,PDO::PARAM_INT);
-$delete_comm->execute();
+<?php
+include("../config/connect.php");
+session_start();
+$sid = $_SESSION['id'];
+$c_id = htmlentities($_POST['cid'], ENT_QUOTES);
+$p_id = htmlentities($_POST['pid'], ENT_QUOTES);
+$delete_comm_sql = "DELETE FROM Comments WHERE c_id= :c_id";
+$delete_comm = $conn->prepare($delete_comm_sql);
+$delete_comm->bindParam(':c_id',$c_id,PDO::PARAM_INT);
+$delete_comm->execute();
 ?>
